e5ee3c7433ee7b285ab5cbd4a891f401306481b4
luren5 <email@luren5.com>
2017-07-21 23:29:33 +0800

    添加新建文件功能

--- cmd/IDE.go
+++ cmd/IDE.go
@@ -37,91 +37,143 @@ var IDECmd = &cobra.Command{
 func startIDE() {
 	r := gin.Default()
 	r.Static("./static", "./IDE")
-	r.LoadHTMLGlob("./IDE/templ/*")
+	r.LoadHTMLGlob("/home/luren5/Project/src/github.com/luren5/mcat/IDE/templ/*")
 	// index
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.templ", gin.H{
-			"title": "hello",
-		})
-	})
-
+	r.GET("/", index)
 	// upload file
-	r.POST("/upload-file", func(c *gin.Context) {
-		_, file, err := c.Request.FormFile("new_sol")
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"status": FAIL,
-				"msg":    err.Error(),
-			})
-		}
-		if !strings.HasSuffix(file.Filename, ".sol") {
-			fmt.Println("file name*******", file.Filename)
-			c.JSON(http.StatusOK, gin.H{
-				"status": FAIL,
-				"msg":    fmt.Sprintf("Invalid file type, %s", file.Filename),
-			})
-			return
-		}
-
-		if f, err := file.Open(); err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"status": FAIL,
-				"msg":    fmt.Sprintf("Failed to open file, %v", err),
-			})
-			return
-		} else {
-			out, err := os.Create(utils.ContractsDir() + "/" + file.Filename)
-			if err != nil {
-				c.JSON(http.StatusOK, gin.H{
-					"status": FAIL,
-					"msg":    fmt.Sprintf("Failed to create file, %v", err),
-				})
-				return
-			}
-			defer out.Close()
-			if _, err := io.Copy(out, f); err != nil {
-				c.JSON(http.StatusOK, gin.H{
-					"status": FAIL,
-					"msg":    fmt.Sprintf("Failed to copy file, %v", err),
-				})
-				return
-			}
-			// redirect
-			c.Redirect(http.StatusTemporaryRedirect, "/edit/"+file.Filename)
-		}
-	})
-
+	r.POST("/upload-file", uploadFile)
 	// edit file
-	r.Any("/edit/:fileName", func(c *gin.Context) {
-		fileName := c.Param("fileName")
-		if _, err := os.Stat(utils.ContractsDir() + "/" + fileName); err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"status": FAIL,
-				"msg":    fmt.Sprintf("Cant't access to file, %v", err),
-			})
-			return
-		}
-		fileContent, err := ioutil.ReadFile(utils.ContractsDir() + "/" + fileName)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"status": FAIL,
-				"msg":    fmt.Sprintf("Failed to get file content, %v", err),
-			})
-			return
-		}
-		c.HTML(http.StatusOK, "index.templ", gin.H{
-			"fileName":    fileName,
-			"fileContent": string(fileContent),
-		})
-	})
+	r.Any("/edit/:fileName", edit)
+	// new file
+	r.GET("/new-file/:fileName", newFile)
 
 	port, err := utils.Config("ide_port")
 	if err != nil {
 		r.Run()
 	}
 	r.Run(":" + port.(string))
+	fmt.Println("IDE is on,listening " + port.(string))
 }
 
 func init() {
 	RootCmd.AddCommand(IDECmd)
+}
+
+// index
+func index(c *gin.Context) {
+	// lis files
+	c.HTML(http.StatusOK, "index.templ", gin.H{
+		"fileSet": getFileSet(),
+	})
+}
+
+// edit
+func edit(c *gin.Context) {
+	fileName := c.Param("fileName")
+	if _, err := os.Stat(utils.ContractsDir() + "/" + fileName); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    fmt.Sprintf("Cant't access to file, %v", err),
+		})
+		return
+	}
+	fileContent, err := ioutil.ReadFile(utils.ContractsDir() + "/" + fileName)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    fmt.Sprintf("Failed to get file content, %v", err),
+		})
+		return
+	}
+	c.HTML(http.StatusOK, "index.templ", gin.H{
+		"fileName":    fileName,
+		"fileContent": string(fileContent),
+		"fileSet":     getFileSet(),
+	})
+}
+
+// upload
+func uploadFile(c *gin.Context) {
+	_, file, err := c.Request.FormFile("new_sol")
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    err.Error(),
+		})
+	}
+	if !strings.HasSuffix(file.Filename, ".sol") {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    fmt.Sprintf("Invalid file type, %s", file.Filename),
+		})
+		return
+	}
+
+	if f, err := file.Open(); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    fmt.Sprintf("Failed to open file, %v", err),
+		})
+		return
+	} else {
+		out, err := os.Create(utils.ContractsDir() + "/" + file.Filename)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": FAIL,
+				"msg":    fmt.Sprintf("Failed to create file, %v", err),
+			})
+			return
+		}
+		defer out.Close()
+		if _, err := io.Copy(out, f); err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": FAIL,
+				"msg":    fmt.Sprintf("Failed to copy file, %v", err),
+			})
+			return
+		}
+		// redirect
+		c.Redirect(http.StatusTemporaryRedirect, "/edit/"+file.Filename)
+	}
+}
+
+// new file
+func newFile(c *gin.Context) {
+	fileName := c.Param("fileName")
+	if !strings.HasSuffix(fileName, ".sol") {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    "Invalid file name.",
+		})
+		return
+	}
+	newFile := utils.ContractsDir() + fileName
+	f, err := os.OpenFile(newFile, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"status": FAIL,
+			"msg":    err.Error(),
+		})
+		return
+	}
+	f.Close()
+	c.JSON(http.StatusOK, gin.H{
+		"status": SUCCESS,
+	})
+}
+
+//get file set
+func getFileSet() []string {
+	files, err := ioutil.ReadDir(utils.ContractsDir())
+	if err != nil {
+		return []string{}
+	}
+	var fileSet []string
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		fileSet = append(fileSet, f.Name())
+	}
+	return fileSet
 }
